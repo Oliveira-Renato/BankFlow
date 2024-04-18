0d2323fd9c1b6384fa53b721ab80917c00a0ab5d
Oliveira-Renato <renato.pritf@gmail.com>
2024-04-18 15:48:58 -0300

    Adicionar arquivos de projeto.

---
+++ BankFlow/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankFlow.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BankFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
