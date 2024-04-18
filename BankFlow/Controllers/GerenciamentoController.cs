1d3b759ab0026289c50f932a50946ac18b0064f2
Oliveira-Renato <renato.pritf@gmail.com>
2024-04-18 17:20:50 -0300

    layout finished

---
+++ BankFlow/Controllers/GerenciamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankFlow.Controllers
+{
+    public class GerenciamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
